33632d313a7330ee0549da36a0fe1c118c24856e
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2023-03-14 17:54:28 +1300

    Create Bank_v2.py

---
+++ Bank_v2.py
@@ -0,0 +1,99 @@
+"""Banking system - version 2
+Edited the displayInfo() function to print out users in neat format
+"""
+
+
+class User:
+    def __init__(self, first_name, last_name, gender, street_address, city,
+                 email, cc_number, cc_type, balance, account_no):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.street_address = street_address
+        self.city = city
+        self.email = email
+        self.cc_number = cc_number
+        self.cc_type = cc_type
+        self.balance = balance
+        self.account_no = account_no
+        userList.append(self)
+
+    def displayInfo(self):
+        print("First Name:", self.first_name)
+        print("Last Name:", self.last_name)
+        print("Gender:", self.gender)
+        print("Street address:", self.street_address)
+        print("City:", self.city)
+        print("Email:", self.email)
+        print("CC Number:", self.cc_number)
+        print("CC Type:", self.cc_type)
+        print("Balance:", self.balance)
+        print("Account Number:", self.account_no)
+
+
+def generateUsers():
+    import csv
+    with open('bankUsers.csv', newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter=',', quotechar="'")
+        for line in filereader:
+            User(line[0], line[1], line[2], line[3], line[4], line[5], line[6],
+                 line[7], float(line[8][1:]), line[9])
+
+
+def findUser():
+    True
+
+
+def overdrafts():
+    # TO COMPLETE
+
+    True
+
+
+def missingEmails():
+    # TO COMPLETE
+
+    True
+
+
+def bankDetails():
+    # TO COMPLETE
+
+    True
+
+
+def transfer():
+    # TO COMPLETE
+
+    True
+
+
+# Main Routine
+userList = []
+
+userChoice = ""
+print("Welcome")
+
+generateUsers()
+while userChoice != "Q":
+    print("What function would you like to run?")
+    print("Type 1 to find a user")
+    print("Type 2 to print overdraft information")
+    print("Type 3 to print users with missing emails")
+    print("Type 4 to print bank details")
+    print("Type 5 to transfer money")
+    print("Type Q to quit")
+    userChoice = input("Enter choice: ")
+    print()
+
+    if userChoice == "1":
+        findUser()
+    elif userChoice == "2":
+        overdrafts()
+    elif userChoice == "3":
+        missingEmails()
+    elif userChoice == "4":
+        bankDetails()
+    elif userChoice == "5":
+        transfer()
+    print()
